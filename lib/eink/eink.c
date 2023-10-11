aa6c53c6460c20037ac5918b36212841b146cca6
amohamdy99 <a.mohamdy99@gmail.com>
2023-10-11 02:20:26 -0700

    Basic Running EPD IT8951 code

---
+++ lib/eink/eink.c
@@ -0,0 +1,91 @@
+#include "eink.h"
+
+#include <time.h> 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+#include "./EPD_IT8951.h"
+#include "../GUI/GUI_Paint.h"
+#include "../GUI/GUI_BMPfile.h"
+#include "../Config/Debug.h"
+
+UBYTE *Refresh_Frame_Buf = NULL;
+
+UBYTE *Panel_Frame_Buf = NULL;
+UBYTE *Panel_Area_Frame_Buf = NULL;
+
+bool Four_Byte_Align = false;
+
+extern int epd_mode;
+extern UWORD VCOM;
+extern UBYTE isColor;
+/******************************************************************************
+function: Change direction of display, Called after Paint_NewImage()
+parameter:
+    mode: display mode
+******************************************************************************/
+static void Epd_Mode(int mode)
+{
+	if(mode == 3) {
+		Paint_SetRotate(ROTATE_0);
+		Paint_SetMirroring(MIRROR_NONE);
+		isColor = 1;
+	}else if(mode == 2) {
+		Paint_SetRotate(ROTATE_0);
+		Paint_SetMirroring(MIRROR_HORIZONTAL);
+	}else if(mode == 1) {
+		Paint_SetRotate(ROTATE_0);
+		Paint_SetMirroring(MIRROR_HORIZONTAL);
+	}else {
+		Paint_SetRotate(ROTATE_0);
+		Paint_SetMirroring(MIRROR_NONE);
+	}
+}
+
+/******************************************************************************
+function: Display_BMP_Example
+parameter:
+    Panel_Width: Width of the panel
+    Panel_Height: Height of the panel
+    Init_Target_Memory_Addr: Memory address of IT8951 target memory address
+    BitsPerPixel: Bits Per Pixel, 2^BitsPerPixel = grayscale
+******************************************************************************/
+UBYTE eInk_BMP(UWORD Panel_Width, UWORD Panel_Height, UDOUBLE Init_Target_Memory_Addr, UBYTE BitsPerPixel, int epd_mode){
+    UWORD WIDTH;
+    if(Four_Byte_Align == true){
+        WIDTH  = Panel_Width - (Panel_Width % 32);
+    }else{
+        WIDTH = Panel_Width;
+    }
+    UWORD HEIGHT = Panel_Height;
+
+    UDOUBLE Imagesize;
+
+    Imagesize = ((WIDTH * BitsPerPixel % 8 == 0)? (WIDTH * BitsPerPixel / 8 ): (WIDTH * BitsPerPixel / 8 + 1)) * HEIGHT;
+    if((Refresh_Frame_Buf = (UBYTE *)malloc(Imagesize)) == NULL) {
+        Debug("Failed to apply for black memory...\r\n");
+        return -1;
+    }
+
+    Paint_NewImage(Refresh_Frame_Buf, WIDTH, HEIGHT, 0, BLACK);
+    Paint_SelectImage(Refresh_Frame_Buf);
+	Epd_Mode(epd_mode);
+    Paint_SetBitsPerPixel(BitsPerPixel);
+    Paint_Clear(WHITE);
+
+    char Path[128] = "./pic/img001.bmp";
+
+    GUI_ReadBmp(Path, 0, 0);
+
+    if(Refresh_Frame_Buf != NULL){
+        free(Refresh_Frame_Buf);
+        Refresh_Frame_Buf = NULL;
+    }
+
+    DEV_Delay_ms(5000);
+
+    return 0;
+}
